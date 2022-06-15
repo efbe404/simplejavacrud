7ac09ad53ab1eca331129c65a537f5c84f0b3098
efbe <>
2022-06-15 19:49:29 +0700

    java-simplecrud

---
+++ src/main/java/javacrud/config.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javacrud;
+
+/**
+ *
+ * @author efbe
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+ 
+public class config {
+    Connection koneksi;
+    
+    public static Connection Koneksi() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/db_coba", "root", "");
+            
+            return koneksi;
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            
+            return null;
+        }
+    }
+}
